850658b653666f8d91e3f08ddcb00743d0b14f8e
Casey Metz <caseymetz@gmail.com>
2016-07-08 12:20:28 -0600

    Add CSS and default range

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-//Variables//
-var recentGuessDisplay = document.getElementById('last-guess');
-var userGuess = document.getElementById('user-guess');
+/* Variables */
+var rangeSubmitButton = document.getElementById('range-submit');
 var guessButton = document.getElementById('guess-button');
 var clearButton = document.getElementById('clear-guess-button');
 var resetButton = document.getElementById('reset-button');
-var rangeSubmitButton = document.getElementById('range-submit');
-var hintMessage = document.getElementById('hint-message');
+var userGuess = document.getElementById('user-guess');
 var userMin = document.getElementById('user-min');
 var userMax = document.getElementById('user-max');
+var recentGuessDisplay = document.getElementById('last-guess');
+var hintMessage = document.getElementById('hint-message');
 var computerChoice = getRandomNum(parseInt(userMin.value), parseInt(userMax.value));
 
-/*Generates computer's choosen number, attaches it to variable.
-(I used used the 'Math.floor +1' method because it gives a better
-distribution of randomness than 'Math.ceil')*/
-function getRandomNum(min,max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+/* Generates computer's choosen number, attaches it to variable. */
+function getRandomNum(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Function which update RNG min/max +/-10 if correct answer is given, as well as updates input fields with innerText call
+function defaultRange(){
+  if (computerChoice = "NaN"){
+    userMin.value = 1;
+    userMax.value = 100;
+  };
+  return userMin.value;
+  return userMax.value;
+}
 
+defaultRange();
+/* Updates min/max +/-10 if correct answer is given and writes new values to screen,
+generates new random number with the new params */
 function levelUp(min, max) {
   min = min-10;
   max = max +10;
@@ -28,13 +37,16 @@ function levelUp(min, max) {
   return computerChoice;
 }
 
-// Submit button for min/max which will feed the RNG & guessButton
+/* Submit button for min/max which will feed the RNG & guessButton */
 rangeSubmitButton.addEventListener('click', function() {
+  if (parseInt(userMin.value) > parseInt(userMax.value)){
+    hintMessage.textContent = "Your minimum can't be higher than your maximum! Pick new parameters."
+  }
   computerChoice = getRandomNum(parseInt(userMin.value), parseInt(userMax.value));
   return computerChoice;
 });
 
-/*Wakes up clear button when text is entered*/
+/*Wakes up clear button when text is entered into guess field*/
 userGuess.addEventListener('input', function(){
   clearButton.disabled = false;
 });
@@ -46,8 +58,7 @@ clearButton.addEventListener('click', function(){
   clearButton.disabled = true;
 });
 
-/*resets game by re-defining computerChoice,
-clearing fields, and disables both buttons*/
+/*resets game by re-defining computerChoice, clearing fields, and disables both buttons*/
 resetButton.addEventListener('click', function(){
   recentGuessDisplay.textContent = "";
   userGuess.value="";
@@ -57,23 +68,30 @@ resetButton.addEventListener('click', function(){
   return computerChoice;
 })
 
-/*guess button displays the guess, triggers error messages and hints,
+/*Displays the guess, triggers error messages and hints, applies LevelUp,
 and initiates game by waking up reset button*/
 guessButton.addEventListener('click', function(){
-  recentGuessDisplay.textContent = userGuess.value;
-  resetButton.disabled= false;
-    if ((parseInt(userGuess.value)) > (parseInt(userMax.value)) ||
-      (parseInt(userGuess.value)) < (parseInt(userMin.value))){
-      hintMessage.textContent = "Error! Your guess is outside the range of potential answers."
-    } else if (parseInt(userGuess.value) < computerChoice){
-      hintMessage.textContent = "Sorry, that number is too low. Try again!"
-    } else if (parseInt(userGuess.value) === computerChoice){
-      hintMessage.textContent = "Congrats! You guessed the number!"
-      levelUp(parseInt(userMin.value), parseInt(userMax.value));
-    } else if (isNaN(parseInt(userGuess.value)) === true){
-      hintMessage.textContent = "Error! That's not a number, silly!"
-    } else if (parseInt(userGuess.value) > computerChoice){
-      hintMessage.textContent = "Sorry, that number is too high. Try again!"
-    };
 
-});
+  computerChoice = getRandomNum(parseInt(userMin.value), parseInt(userMax.value));
+
+  recentGuessDisplay.textContent = userGuess.value;
+
+  resetButton.disabled = false;
+
+  if ((parseInt(userGuess.value)) > (parseInt(userMax.value)) || (parseInt(userGuess.value)) < (parseInt(userMin.value))){
+    hintMessage.textContent = "Error! Your guess is outside the range of potential answers.";
+
+    }else if (parseInt(userGuess.value) === computerChoice){
+      hintMessage.textContent = "Congrats! You guessed the number!";
+      levelUp(parseInt(userMin.value), parseInt(userMax.value));
+
+    }else if (isNaN(parseInt(userGuess.value)) === true){
+      hintMessage.textContent = "Error! That's not a number, silly!";
+
+    }else if (parseInt(userGuess.value) < computerChoice){
+      hintMessage.textContent = "Sorry, that number is too low. Try again!";
+
+    }else if (parseInt(userGuess.value) > computerChoice){
+      hintMessage.textContent = "Sorry, that number is too high. Try again!";};
+
+  });
